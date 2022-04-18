d8a89df927892fd0444f70fd86a3bffc17336c87
Devin <devincapriola@gmail.com>
2022-04-17 22:27:48 -0400

    init

---
+++ glide_finetune/glide_finetune.py
@@ -0,0 +1,132 @@
+import os
+from typing import Tuple
+
+import torch as th
+from glide_text2im.respace import SpacedDiffusion
+from glide_text2im.text2im_model import Text2ImUNet
+from tqdm import tqdm
+from wandb import wandb
+
+from glide_finetune import glide_util, train_util
+
+def base_train_step(
+    glide_model: Text2ImUNet,
+    glide_diffusion: SpacedDiffusion,
+    batch: Tuple[th.Tensor, th.Tensor, th.Tensor],
+    device: str,
+):
+    """
+    Perform a single training step.
+
+        Args:
+            glide_model: The model to train.
+            glide_diffusion: The diffusion to use.
+            batch: A tuple of (tokens, masks, reals) where tokens is a tensor of shape (batch_size, seq_len), masks is a tensor of shape (batch_size, seq_len) and reals is a tensor of shape (batch_size, 3, side_x, side_y) normalized to [-1, 1].
+            device: The device to use for getting model outputs and computing loss.
+        Returns:
+            The loss.
+    """
+    tokens, masks, reals = [x.to(device) for x in batch]
+    timesteps = th.randint(
+        0, len(glide_diffusion.betas) - 1, (reals.shape[0],), device=device
+    )
+    noise = th.randn_like(reals, device=device)
+    x_t = glide_diffusion.q_sample(reals, timesteps, noise=noise).to(device)
+    _, C = x_t.shape[:2]
+    model_output = glide_model(
+        x_t.to(device),
+        timesteps.to(device),
+        tokens=tokens.to(device),
+        mask=masks.to(device),
+    )
+    epsilon, _ = th.split(model_output, C, dim=1)
+    return th.nn.functional.mse_loss(epsilon, noise.to(device).detach())
+
+def upsample_train_step(
+    glide_model: Text2ImUNet,
+    glide_diffusion: SpacedDiffusion,
+    batch: Tuple[th.Tensor, th.Tensor, th.Tensor, th.Tensor],
+    device: str,
+):
+    """
+    Perform a single training step.
+
+        Args:
+            glide_model: The model to train.
+            glide_diffusion: The diffusion to use.
+            batch: A tuple of (tokens, masks, low_res, high_res) where 
+                - tokens is a tensor of shape (batch_size, seq_len), 
+                - masks is a tensor of shape (batch_size, seq_len) with dtype torch.bool
+                - low_res is a tensor of shape (batch_size, 3, base_x, base_y), normalized to [-1, 1]
+                - high_res is a tensor of shape (batch_size, 3, base_x*4, base_y*4), normalized to [-1, 1]
+            device: The device to use for getting model outputs and computing loss.
+        Returns:
+            The loss.
+    """
+    tokens, masks, low_res_image, high_res_image = [ x.to(device) for x in batch ]
+    timesteps = th.randint(0, len(glide_diffusion.betas) - 1, (low_res_image.shape[0],), device=device)
+    noise = th.randn_like(high_res_image, device=device) # Noise should be shape of output i think
+    noised_high_res_image = glide_diffusion.q_sample(high_res_image, timesteps, noise=noise).to(device)
+    _, C = noised_high_res_image.shape[:2]
+    model_output = glide_model(
+        noised_high_res_image.to(device),
+        timesteps.to(device),
+        low_res=low_res_image.to(device),
+        tokens=tokens.to(device),
+        mask=masks.to(device))
+    epsilon, _ = th.split(model_output, C, dim=1)
+    return th.nn.functional.mse_loss(epsilon, noise.to(device).detach())
+
+
+def run_glide_finetune_epoch(
+    glide_model: Text2ImUNet,
+    glide_diffusion: SpacedDiffusion,
+    glide_options: dict,
+    dataloader: th.utils.data.DataLoader,
+    optimizer: th.optim.Optimizer,
+    sample_bs: int,  # batch size for inference
+    sample_gs: float = 4.0,  # guidance scale for inference
+    sample_respacing: str = '100', # respacing for inference
+    prompt: str = "",  # prompt for inference, not training
+    side_x: int = 64,
+    side_y: int = 64,
+    outputs_dir: str = "./outputs",
+    checkpoints_dir: str = "./finetune_checkpoints",
+    device: str = "cpu",
+    log_frequency: int = 100,
+    wandb_run=None,
+    gradient_accumualation_steps=1,
+    epoch: int = 0,
+    train_upsample: bool = False,
+    upsample_factor=4,
+    image_to_upsample='low_res_face.png',
+):
+    if train_upsample: train_step = upsample_train_step
+    else: train_step = base_train_step
+
+    os.makedirs(checkpoints_dir, exist_ok=True)
+    glide_model.to(device)
+    glide_model.train()
+    log = {}
+    for train_idx, batch in tqdm(enumerate(dataloader)):
+        accumulated_loss = train_step(
+            glide_model=glide_model,
+            glide_diffusion=glide_diffusion,
+            batch=batch,
+            device=device,
+        )
+        accumulated_loss.backward()
+        optimizer.step()
+        glide_model.zero_grad()
+        log = {**log, "iter": train_idx, "loss": accumulated_loss.item() / gradient_accumualation_steps}
+        tqdm.write(f"loss: {accumulated_loss.item():.4f}")
+        # Sample from the model removed due to colab errors.
+        
+        if train_idx % 5000 == 0 and train_idx > 0:
+            train_util.save_model(glide_model, checkpoints_dir, train_idx, epoch)
+            tqdm.write(
+                f"Saved checkpoint {train_idx} to {checkpoints_dir}/glide-ft-{train_idx}.pt"
+            )
+        wandb_run.log(log)
+    tqdm.write(f"Finished training, saving final checkpoint")
+    train_util.save_model(glide_model, checkpoints_dir, train_idx, epoch)
---
+++ train_glide.py
@@ -0,0 +1,320 @@
+import argparse
+from glob import glob
+import os
+
+import numpy as np
+import torch as th
+import torchvision.transforms as T
+from tqdm import trange
+
+from glide_finetune.glide_finetune import run_glide_finetune_epoch
+from glide_finetune.glide_util import load_model
+from glide_finetune.loader import TextImageDataset
+from glide_finetune.train_util import wandb_setup
+from glide_finetune.wds_loader import glide_wds_loader
+
+
+def run_glide_finetune(
+    data_dir="/content/data",
+    batch_size=1,
+    learning_rate=1e-5,
+    adam_weight_decay=0.0,
+    side_x=64,
+    side_y=64,
+    resize_ratio=1.0,
+    uncond_p=0.0,
+    resume_ckpt="",
+    checkpoints_dir="./finetune_checkpoints",
+    use_fp16=False,  # Tends to cause issues,not sure why as the paper states fp16 is stable.
+    device="cpu",
+    freeze_transformer=False,
+    freeze_diffusion=False,
+    project_name="glide_finetune",
+    activation_checkpointing=False,
+    use_captions=True,
+    num_epochs=100,
+    log_frequency=100,
+    test_prompt="a group of skiers are preparing to ski down a mountain.",
+    sample_bs=1,
+    sample_gs=8.0,
+    use_webdataset=False,
+    image_key="jpg",
+    caption_key="txt",
+    enable_upsample=False,
+    upsample_factor=4,
+    image_to_upsample='low_res_face.png',
+):
+    if "~" in data_dir:
+        data_dir = os.path.expanduser(data_dir)
+    if "~" in checkpoints_dir:
+        checkpoints_dir = os.path.expanduser(checkpoints_dir)
+
+    # Create the checkpoint/output directories
+    os.makedirs(checkpoints_dir, exist_ok=True)
+
+    # Start wandb logging
+    wandb_run = wandb_setup(
+        batch_size=batch_size,
+        side_x=side_x,
+        side_y=side_y,
+        learning_rate=learning_rate,
+        use_fp16=use_fp16,
+        device=device,
+        data_dir=data_dir,
+        base_dir=checkpoints_dir,
+        project_name=project_name,
+    )
+    print("Wandb setup.")
+
+    # Model setup
+    glide_model, glide_diffusion, glide_options = load_model(
+        glide_path=resume_ckpt,
+        use_fp16=use_fp16,
+        freeze_transformer=freeze_transformer,
+        freeze_diffusion=freeze_diffusion,
+        activation_checkpointing=activation_checkpointing,
+        model_type="base" if not enable_upsample else "upsample",
+    )
+    glide_model.train()
+    number_of_params = sum(x.numel() for x in glide_model.parameters())
+    print(f"Number of parameters: {number_of_params}")
+    number_of_trainable_params = sum(
+        x.numel() for x in glide_model.parameters() if x.requires_grad
+    )
+    print(f"Trainable parameters: {number_of_trainable_params}")
+
+    # Data setup
+    print("Loading data...")
+    if use_webdataset:
+        dataset = glide_wds_loader(
+            urls=data_dir,
+            caption_key=caption_key,
+            image_key=image_key,
+            enable_image=True,
+            enable_text=use_captions,
+            enable_upsample=enable_upsample,
+            tokenizer=glide_model.tokenizer,
+            ar_lower=0.5,
+            ar_upper=2.0,
+            min_original_height=side_x * upsample_factor,
+            min_original_width=side_y * upsample_factor,
+            upscale_factor=upsample_factor,
+            nsfw_filter=True,
+            similarity_threshold_upper=0.0,
+            similarity_threshold_lower=0.5,
+            words_to_skip=[],
+            dataset_name="laion",  # can be laion, alamy.
+        )
+    else:
+        dataset = TextImageDataset(
+            folder=data_dir,
+            side_x=side_x,
+            side_y=side_y,
+            resize_ratio=resize_ratio,
+            uncond_p=uncond_p,
+            shuffle=True,
+            tokenizer=glide_model.tokenizer,
+            text_ctx_len=glide_options["text_ctx"],
+            use_captions=use_captions,
+            enable_glide_upsample=enable_upsample,
+            upscale_factor=upsample_factor,  # TODO: make this a parameter
+        )
+
+    # Data loader setup
+    dataloader = th.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=not use_webdataset,
+        num_workers=0,
+        pin_memory=(device == "cuda"),
+    )
+
+    # Optimizer setup
+    optimizer = th.optim.AdamW(
+        [x for x in glide_model.parameters() if x.requires_grad],
+        lr=learning_rate,
+        weight_decay=adam_weight_decay,
+    )
+
+    # Training setup
+    outputs_dir = "./outputs"
+    os.makedirs(outputs_dir, exist_ok=True)
+    for epoch in trange(num_epochs):
+        print(f"Starting epoch {epoch}")
+        run_glide_finetune_epoch(
+            glide_model=glide_model,
+            glide_diffusion=glide_diffusion,
+            glide_options=glide_options,
+            optimizer=optimizer,
+            dataloader=dataloader,
+            prompt=test_prompt,
+            sample_bs=sample_bs,
+            sample_gs=sample_gs,
+            checkpoints_dir=checkpoints_dir,
+            outputs_dir=outputs_dir,
+            side_x=side_x,
+            side_y=side_y,
+            device=device,
+            wandb_run=wandb_run,
+            log_frequency=log_frequency,
+            epoch=epoch,
+            gradient_accumualation_steps=1,
+            train_upsample=enable_upsample,
+        )
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", "-data", type=str, default="./data")
+    parser.add_argument("--batch_size", "-bs", type=int, default=1)
+    parser.add_argument("--learning_rate", "-lr", type=float, default=2e-5)
+    parser.add_argument("--adam_weight_decay", "-adam_wd", type=float, default=0.0)
+    parser.add_argument("--side_x", "-x", type=int, default=64)
+    parser.add_argument("--side_y", "-y", type=int, default=64)
+    parser.add_argument(
+        "--resize_ratio", "-crop", type=float, default=0.8, help="Crop ratio"
+    )
+    parser.add_argument(
+        "--uncond_p",
+        "-p",
+        type=float,
+        default=0.2,
+        help="Probability of using the empty/unconditional token instead of a caption. OpenAI used 0.2 for their finetune.",
+    )
+    parser.add_argument(
+        "--train_upsample",
+        "-upsample",
+        action="store_true",
+        help="Train the upsampling type of the model instead of the base model.",
+    )
+    parser.add_argument(
+        "--resume_ckpt",
+        "-resume",
+        type=str,
+        default="",
+        help="Checkpoint to resume from",
+    )
+    parser.add_argument(
+        "--checkpoints_dir", "-ckpt", type=str, default="./glide_checkpoints/"
+    )
+    parser.add_argument("--use_fp16", "-fp16", action="store_true")
+    parser.add_argument("--device", "-dev", type=str, default="")
+    parser.add_argument("--log_frequency", "-freq", type=int, default=100)
+    parser.add_argument("--freeze_transformer", "-fz_xt", action="store_true")
+    parser.add_argument("--freeze_diffusion", "-fz_unet", action="store_true")
+    parser.add_argument("--project_name", "-name", type=str, default="glide-finetune")
+    parser.add_argument("--activation_checkpointing", "-grad_ckpt", action="store_true")
+    parser.add_argument("--use_captions", "-txt", action="store_true")
+    parser.add_argument("--epochs", "-epochs", type=int, default=20)
+    parser.add_argument(
+        "--test_prompt",
+        "-prompt",
+        type=str,
+        default="a group of skiers are preparing to ski down a mountain.",
+    )
+    parser.add_argument(
+        "--test_batch_size",
+        "-tbs",
+        type=int,
+        default=1,
+        help="Batch size used for model eval, not training.",
+    )
+    parser.add_argument(
+        "--test_guidance_scale",
+        "-tgs",
+        type=float,
+        default=1.0,
+        help="Guidance scale used during model eval, not training.",
+    )
+    parser.add_argument(
+        "--use_webdataset",
+        "-wds",
+        action="store_true",
+        help="Enables webdataset (tar) loading",
+    )
+    parser.add_argument(
+        "--wds_image_key",
+        "-wds_img",
+        type=str,
+        default="jpg",
+        help="A 'key' e.g. 'jpg' used to access the image in the webdataset",
+    )
+    parser.add_argument(
+        "--wds_caption_key",
+        "-wds_cap",
+        type=str,
+        default="txt",
+        help="A 'key' e.g. 'txt' used to access the caption in the webdataset",
+    )
+    parser.add_argument(
+        "--wds_dataset_name",
+        "-wds_name",
+        type=str,
+        default="laion",
+        help="Name of the webdataset to use (laion or alamy)",
+    )
+    parser.add_argument("--seed", "-seed", type=int, default=0)
+    parser.add_argument(
+        "--cudnn_benchmark",
+        "-cudnn",
+        action="store_true",
+        help="Enable cudnn benchmarking. May improve performance. (may not)",
+    )
+    parser.add_argument(
+        "--upscale_factor", "-upscale", type=int, default=4, help="Upscale factor for training the upsampling model only"
+    )
+    parser.add_argument("--image_to_upsample", "-lowres", type=str, default="low_res_face.png")
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    # CUDA/CPU setup
+    args = parse_args()
+    if len(args.device) > 0:
+        device = th.device(args.device)
+    else:
+        device = th.device("cpu") if not th.cuda.is_available() else th.device("cuda")
+
+    th.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    th.backends.cudnn.benchmark = args.cudnn_benchmark
+
+    for arg in vars(args):
+        print(f"--{arg} {getattr(args, arg)}")
+
+    if args.use_webdataset:
+        # webdataset uses tars
+        data_dir = glob(os.path.join(args.data_dir, "*.tar"))
+    
+    run_glide_finetune(
+        data_dir='/content/data',
+        batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        adam_weight_decay=args.adam_weight_decay,
+        side_x=args.side_x,
+        side_y=args.side_y,
+        resize_ratio=args.resize_ratio,
+        uncond_p=args.uncond_p,
+        resume_ckpt=args.resume_ckpt,
+        checkpoints_dir=args.checkpoints_dir,
+        use_fp16=args.use_fp16,
+        device=device,
+        log_frequency=args.log_frequency,
+        freeze_transformer=args.freeze_transformer,
+        freeze_diffusion=args.freeze_diffusion,
+        project_name=args.project_name,
+        activation_checkpointing=args.activation_checkpointing,
+        use_captions=args.use_captions,
+        num_epochs=args.epochs,
+        test_prompt=args.test_prompt,
+        sample_bs=args.test_batch_size,
+        sample_gs=args.test_guidance_scale,
+        use_webdataset=args.use_webdataset,
+        image_key=args.wds_image_key,
+        caption_key=args.wds_caption_key,
+        enable_upsample=args.train_upsample,
+        upsample_factor=args.upscale_factor,
+        image_to_upsample=args.image_to_upsample,
+    )
